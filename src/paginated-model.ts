8dbe776498e482be6bfc35ce603b87af6d9eb8e6
maddocnc <maddocnc@gmail.com>
2023-10-23 18:03:10 +0300

    fix(types): fix types

--- src/paginated-model.ts
+++ src/paginated-model.ts
@@ -1,10 +1,25 @@
-import {PaginatedResult} from "./paginated-result";
-import {PaginationOptions} from "./pagination-options";
-import {Model} from "mongoose";
+import {
+  type AggregateOptions,
+  type FilterQuery,
+  type Model,
+  type PipelineStage,
+  type ProjectionType,
+  type QueryOptions,
+} from 'mongoose'
+
+import { type PaginatedResult } from './paginated-result'
+import { type PaginationOptions } from './pagination-options'
 
 export interface PaginatedModel<T> extends Model<T> {
-  paginate(
-    options?: PaginationOptions<T>,
-    onError?: Function
-    ): Promise<PaginatedResult<T> | undefined>
+  findPaginated(
+    filter: FilterQuery<T>,
+    pagination: PaginationOptions<T>,
+    projection?: ProjectionType<T> | null,
+    options?: QueryOptions<T> | null
+  ): Promise<PaginatedResult<T>>
+  aggregatePaginated(
+    pipeline: PipelineStage[],
+    paginate: PaginationOptions<T>,
+    options?: AggregateOptions
+  ): Promise<PaginatedResult<T>>
 }
